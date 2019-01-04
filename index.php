eb7ec7141c2ccb8aa08eef4b0e842e9cedfa2264
SHAJ <shakilahmedshaj@gmail.com>
2019-01-05 01:06:29 +0600

    insert and db config done

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+
+require 'dbconfig/config.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -21,11 +27,11 @@
                 <label><b>Product Name</b></label>
                 <input type="text" placeholder="Enter Product Name" name="pname">
                 <label><b>Product Brand</b></label>
-                <input type="text" placeholder="Enter Product Brand" name="pbrand" >
+                <input type="text" placeholder="Enter Product Brand" name="pbrand">
                 <label><b>Product Cost</b></label>
-                <input type="number" placeholder="Enter Product Cost" name="pcost" >
+                <input type="number" placeholder="Enter Product Cost" name="pcost">
                 <label><b>Product Quantity</b></label><br>
-                <input type="number" placeholder="Enter Product Quantity" name="pquantity" >
+                <input type="number" placeholder="Enter Product Quantity" name="pquantity">
 
                 <center>
                     <button id="btn_insert" name="insert_btn" type="submit">Insert</button>
@@ -33,33 +39,37 @@
                     <button id="btn_delete" name="delete_btn" type="submit">Delete</button>
                 </center>
             </form>
+            <?php
+            
+            if(isset($_POST['insert_btn'])){
+
+                @$pid=$_POST['pid'];
+                @$pname=$_POST['pname'];
+                @$pbrand=$_POST['pbrand'];
+                @$pcost=$_POST['pcost'];
+                @$pquantity=$_POST['pquantity'];
 
 
+                if($pid=="" || $pname=="" || $pbrand=="" || $pcost=="" || $pquantity=="")
+                {
+                    echo '<script type="text/javascript">alert("Insert values in all fields")</script>';
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
+                else{
+                    $query = "insert into productsinfotbl values($pid,'$pname','$pbrand',$pcost,$pquantity)";
+                    $query_run=mysqli_query($con,$query);
+                    if($query_run)
+                    {
+                            echo '<script type="text/javascript">alert("Values inserted successfully")</script>';
+                    }
+                    else{
+                        echo '<script type="text/javascript">alert("Values Not inserted")</script>';
+                    }
+                }
+            }
+            ?>
         </div>
-
-
-
-
-
-
-
-
     </div>
-
-
 </body>
 
 </html>
